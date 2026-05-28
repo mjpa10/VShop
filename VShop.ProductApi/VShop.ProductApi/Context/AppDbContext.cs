543b09df1b4ef685acb1201f577c144490d6f10d
mtheu <m.theus.jose.pereira@gmail.com>
2026-05-28 11:19:43 -0300

    Implementação da Fluent API no DbContext

--- VShop.ProductApi/VShop.ProductApi/Context/AppDbContext.cs
+++ VShop.ProductApi/VShop.ProductApi/Context/AppDbContext.cs
@@ -10,4 +10,17 @@ public class AppDbContext : DbContext
     public DbSet<Product>? Products { get; set; }  // o modelo se chama Product, e a tabela se chama Products, é uma convenção do Entity Framework.
     public DbSet<Category>? Categories { get; set; } // o modelo se chama Category, e a tabela se chama Categories, é uma convenção do Entity Framework.
 
+    //fluent API para configurar o relacionamento entre as entidades Product e Category
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // mapeamento da entidade Product(é parecido com data annotations)
+        modelBuilder.Entity<Category>().
+            Property(c => c.Name).
+            IsRequired().
+            HasMaxLength(100); // o nome do produto é obrigatório e tem um tamanho máximo de 100 caracteres
+
+        modelBuilder.Entity<Product>().
+            Property(c => c.Price).
+            HasPrecision(12, 2); // o preço do produto tem uma precisão de 12 dígitos e 2 casas decimais
+    }
 }
