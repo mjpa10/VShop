b1f00381869c7e7dae4ca8fe180e6d53dc159d3e
mtheu <m.theus.jose.pereira@gmail.com>
2026-05-28 13:59:05 -0300

    Fluent API e migrations

--- VShop.ProductApi/VShop.ProductApi/Context/AppDbContext.cs
+++ VShop.ProductApi/VShop.ProductApi/Context/AppDbContext.cs
@@ -5,22 +5,61 @@ namespace VShop.ProductApi.Context;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<DbContext> options) : base(options) { }
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     // DbSet é uma coleção de entidades do tipo Product, que representa a tabela de produtos no banco de dados.
     public DbSet<Product>? Products { get; set; }  // o modelo se chama Product, e a tabela se chama Products, é uma convenção do Entity Framework.
     public DbSet<Category>? Categories { get; set; } // o modelo se chama Category, e a tabela se chama Categories, é uma convenção do Entity Framework.
 
     //fluent API para configurar o relacionamento entre as entidades Product e Category
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    protected override void OnModelCreating(ModelBuilder mb)
     {
-        // mapeamento da entidade Product(é parecido com data annotations)
-        modelBuilder.Entity<Category>().
+        // mapeamento da entidade Category(é parecido com data annotations)
+        mb.Entity<Category>().HasKey(c => c.CategoryId); // a chave primária da entidade Category é a propriedade CategoryId)
+
+        mb.Entity<Category>().
             Property(c => c.Name).
             IsRequired().
             HasMaxLength(100); // o nome do produto é obrigatório e tem um tamanho máximo de 100 caracteres
 
-        modelBuilder.Entity<Product>().
+        //Product
+        mb.Entity<Product>().
+            Property(c => c.Name).
+            IsRequired().
+            HasMaxLength(100);
+
+        mb.Entity<Product>().
+          Property(c => c.Description).
+          IsRequired().
+          HasMaxLength(255);
+
+        mb.Entity<Product>().
+         Property(c => c.ImageURL).
+         IsRequired().
+         HasMaxLength(255);
+
+        mb.Entity<Product>().
             Property(c => c.Price).
             HasPrecision(12, 2); // o preço do produto tem uma precisão de 12 dígitos e 2 casas decimais
+
+        mb.Entity<Category>().
+            HasMany(g => g.Products).
+            WithOne(c => c.Category).
+            IsRequired().
+            OnDelete(DeleteBehavior.Cascade);
+        // um para muitos, uma categoria tem muitos produtos, um produto tem uma categoria, a exclusão em cascata significa que quando uma categoria for excluída,
+        // todos os produtos relacionados a ela também serão excluídos.)
+
+        mb.Entity<Category>().HasData(
+            new Category
+            {
+                CategoryId = 1,
+                Name = "Material Escolar"
+            },
+            new Category
+            {
+                CategoryId = 2,
+                Name = "Acessórios"
+            }
+         );
     }
 }
---
+++ VShop.ProductApi/VShop.ProductApi/Migrations/20260528145245_SeedProducts.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VShop.ProductApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("Insert into products(Name, Price, Description, Stock, ImageURL, CategoryId)" +
+                "values('Caderno',7.55, 'caderno', 10, 'caderno1.jpg',1)");
+
+            mb.Sql("Insert into products(Name, Price, Description, Stock, ImageURL, CategoryId)" +
+                "values('Caneta',2.50, 'caneta azul', 25, 'caneta1.jpg',1)");
+
+            mb.Sql("Insert into products(Name, Price, Description, Stock, ImageURL, CategoryId)" +
+                "values('Borracha',1.75, 'borracha branca', 15, 'borracha1.jpg',1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("Delete from products");
+        }
+    }
+}
