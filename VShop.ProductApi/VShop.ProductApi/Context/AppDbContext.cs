241e7177f09a244fdd7020801fd2578dac4fc0f1
mtheu <m.theus.jose.pereira@gmail.com>
2026-05-28 14:58:51 -0300

    Adicionado CategoryRepository

--- VShop.ProductApi/VShop.ProductApi/Context/AppDbContext.cs
+++ VShop.ProductApi/VShop.ProductApi/Context/AppDbContext.cs
@@ -7,8 +7,8 @@ public class AppDbContext : DbContext
 {
     public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     // DbSet é uma coleção de entidades do tipo Product, que representa a tabela de produtos no banco de dados.
-    public DbSet<Product>? Products { get; set; }  // o modelo se chama Product, e a tabela se chama Products, é uma convenção do Entity Framework.
-    public DbSet<Category>? Categories { get; set; } // o modelo se chama Category, e a tabela se chama Categories, é uma convenção do Entity Framework.
+    public DbSet<Product> Products { get; set; }  // o modelo se chama Product, e a tabela se chama Products, é uma convenção do Entity Framework.
+    public DbSet<Category> Categories { get; set; } // o modelo se chama Category, e a tabela se chama Categories, é uma convenção do Entity Framework.
 
     //fluent API para configurar o relacionamento entre as entidades Product e Category
     protected override void OnModelCreating(ModelBuilder mb)
